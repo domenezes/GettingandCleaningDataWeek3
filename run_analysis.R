8d01688350250d9a04687f69ce2e3dfca796a614
domenezes <domen.rj@gmail.com>
2015-07-26 15:18:51 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,14 +1,14 @@
 print(getwd())
-setwd("C:/Users/Diego/learningR/coursera/Getting and Cleaning Data/week 3/Course Project")
+setwd("C:/R/coursera/Getting and Cleaning Data/week 3/Course Project")
 print(getwd())
 
 
-# temp <- tempfile()
-# download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",temp)
-# for (i in temp) unzip(i)
-# unlink(temp)
+temp <- tempfile()
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",temp)
+for (i in temp) unzip(i)
+unlink(temp)
 
-
+#PART 1 - MERGING TRAINING AND TEST DATA SETS:
 x_train <- read.table("./UCI HAR Dataset/train/X_train.txt", header = FALSE)
 x_test <- read.table("./UCI HAR Dataset/test/X_test.txt", header = FALSE)
 y_train <- read.table("./UCI HAR Dataset/train/y_train.txt", header = FALSE)
@@ -22,44 +22,37 @@ subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt", header = F
 # print(dim(y_test))
 #print(dim(subject_train))
 #print(dim(subject_test))
-# 
-# 
-# print(str(x_train))
-# print(str(y_train))
-# print(str(subject_train))
 
 X <- rbind(x_train, x_test)
 Y <- rbind(y_train, y_test)
 subj <- rbind(subject_train, subject_test)
 
+#PART 2 - EXTRACT ONLY THE MEASUREMENTS ON THE MEAN AND SD: (my friend suggested do this for part 2)
 features <- read.table("./UCI HAR Dataset/features.txt")
-names(features) <- c('feat_id', 'feat_name')
+names(features) <- c('feature_id', 'feature_name')
 
-# print(str(subj)[1])
-# print(str(subj)[1000000])
-activities <- read.table("./UCI HAR Dataset/activity_labels.txt")
-colnames(activities) <- c('rowID', 'exercise')
-
-index_features <- grep("-mean\\(\\)|-std\\(\\)", features$feat_name) 
+index_features <- grep("-mean\\(\\)|-std\\(\\)", features$feature_name) 
 X <- X[, index_features] 
 names(X) <- gsub("\\(|\\)", "", (features[index_features, 2]))
 
 
-#print(dim(activities[Y[, 1], 2]))
-#Y[, 1] = activities[Y[, 1], 2]
+#PART 3 - NAMING ACTIVITIES IN THE ACTIVITIES DATA SET:
+activities <- read.table("./UCI HAR Dataset/activity_labels.txt")
+colnames(activities) <- c('exercise_id', 'exercise_name')
 
+#print(dim(activities[Y[, 1], 2])) 
+#- my friend suggested the line below to properly the kinds of activities/exercises
+#to each column
+Y[, 1] = activities[Y[, 1], 2]
+
+#PART 4 - LABELLING DATA SET PROPERLY:
 names(Y) <- "Exercise"
 names(subj) <- "Subject"
 
-
+#combining X, Y and subj by columns
 combined <- cbind(subj, Y, X)
-# print(names(combined))
-# print(dim(combined))
-# print(names(activities))
-# print(dim(activities))
-# print(str(activities))
 
-print(length(3:dim(combined)[2]))
+#PART 5 - CREATING A TIDY DATA SET WITH THE AVERAGE OF EACH VARIABLE FOR EACH ACTIVITY AND SUBJECT:
 print(length(3:ncol(combined)))
 
 combined_with_average <- aggregate(combined[,3:ncol(combined)],list(combined$Subject, combined$Exercise), mean)
@@ -67,8 +60,6 @@ combined_with_average <- aggregate(combined[,3:ncol(combined)],list(combined$Sub
 colnames(combined_with_average)[1] <- "Subject"
 colnames(combined_with_average)[2] <- "Exercise"
 
-#print(names(combined_with_average))
-#print(dim(combined_with_average))
-write.table(combined_with_average, "tidyDataFileAVGtxt.txt",row.name=FALSE)
-#print(dim(tidyDataFileAVGtxt.txt))
+write.table(combined_with_average, "FinalTidyDataSet.txt",row.name=FALSE)
+
 
